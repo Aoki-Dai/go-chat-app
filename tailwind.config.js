b90d1a27ade6de31e8cca4a940c9a60716381874
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-09-16 23:59:57 +0900

    node_modulesをgitに上げないようにしたい

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./main.go",
+    "./style.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
